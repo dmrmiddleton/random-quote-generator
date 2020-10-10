4392bf6377b8337ec229e7ae8719fad96b3711fe
Dave Middleton <dmrmiddleton@outlook.com>
2020-10-10 16:55:50 +0100

    Multidimensional array of quotes created

--- inc/functions.php
+++ inc/functions.php
@@ -1,8 +1,18 @@
 // PHP - Random Quote Generator
+//Create PHP open tag
+<?php
 
-// Create the Multidimensional array of quote elements and name it quotes
-// Each inner array element should be an associative array
-
+// Multidimensional array to store motivational quotes and associated information.
+// Each array within the multidimensional array contains a quote and source with optional elements for citation and year.
+$quotes = [
+          ["quote" => "Creativity is just connecting things.", "source" => "Steve Jobs", "citation" => "WIRED", "year" => "1996"],
+          ["quote" => "The greatest glory in living lies not in never falling, but in rising every time we fall.", "source" => "Nelson Mandela", "citation" => "Long Walk to Freedom", "year" => "1995"],
+          ["quote" => "Love the life you live. Live the life you love.", "source" => "Bob Marley"],
+          ["quote" => "The way to get started is to quit talking and begin doing.", "source" => "Walt Disney"],
+          ["quote" => "Every strike brings me closer to the next home run.", "source" => "Babe Ruth", "citation" => "Weird Ideas That Work", "year" => "2001"],
+          ["quote" => "You will face many defeats in life, but never let yourself be defeated.", "source" => "Maya Angelou"],
+          ["quote" => "TThe future belongs to those who believe in the beauty of their dreams.", "source" => "Eleanor Roosevelt"],
+          ];
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
@@ -10,3 +20,5 @@
 
 
 // Create the printQuote funtion and name it printQuote
+
+?>
