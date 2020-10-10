1791cf0221103ce918f015992699e18f8fecbb7c
Dave Middleton <dmrmiddleton@outlook.com>
2020-10-10 21:26:52 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # random quote generator
  PHP Techdegree Project 1
+
+An app that displays random famous quotes each time the button is clicked or the page is refreshed.
--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <!-- EXTRA CREDIT - http-equiv attribute added to autmoatically refresh the page every 5 seconds -->
-  <meta http-equiv="refresh" content="5">
+  <!-- EXTRA CREDIT - http-equiv attribute added to autmoatically refresh the page every 10 seconds -->
+  <meta http-equiv="refresh" content="10">
   <title>Random Quotes</title>
   <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="css/normalize.css">
